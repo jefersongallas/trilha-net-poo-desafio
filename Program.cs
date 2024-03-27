145fff691da851dce348a4c08c64524df518ca57
jefersongallas <jefersongallas@gmail.com>
2024-03-26 21:08:21 -0300

    Commit com as alterações para o desafio do celular da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iniciando Testes NOKIA!\n");
+Nokia nokia = new Nokia("123456789", "Modelo do Nokia", "Imei do Nokia", 8);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("App no Nokia");
+
+Console.WriteLine("\n\nIniciando Testes IPHONE!\n");
+Iphone iphone = new Iphone("987654321", "Modelo do Iphone", "Imei do Iphone", 16);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("App no Iphone");
